d3760cef6d157d22bd5a3a8ff364700098de6256
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-02-03 15:25:38 +0300

    fixed memory limit

--- sandbox/run/init.sh
+++ sandbox/run/init.sh
@@ -3,6 +3,6 @@ if [ "$(id -u)" = "0" ]; then
     echo "container cannot be run as root"
     exit 1
 fi
-export PATH=/opt/bin:/sandbox:$PATH
-ulimit -v "$MEMORY_RUN"
-timeout -s KILL "$TIMEOUT_RUN" "$@"
+export PATH=/opt/bin:/in:$PATH
+ulimit -v 524288
+timeout -s KILL $TIMEOUT "$@"
