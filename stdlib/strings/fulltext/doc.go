d41e2f4a091f8e8bf1ee69e5115f81038b61aa02
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-04-24 12:05:47 +0300

    fix fulltext doc

--- stdlib/strings/fulltext/doc.go
+++ stdlib/strings/fulltext/doc.go
@@ -4,6 +4,6 @@ Given a list of "documents", the package implements an extremely simple fulltext
 It allows to find documents containing all words from the query, not necessarily in the given order.
 
 The lack of requirement on matching the word order is important.
-It enables a trivial implementation _without_ specialised algorithms or data structures (such as suffix trees/arrays).
+It enables a trivial implementation without specialised algorithms or data structures (such as suffix trees/arrays).
 */
 package fulltext
